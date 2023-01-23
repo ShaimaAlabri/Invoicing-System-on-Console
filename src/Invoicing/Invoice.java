83261a1ae35faca5aa947c99dda6aeb790dd74ae
unknown <shaima96alabri@hotmail.com>
2023-01-22 23:06:38 -0800

    Chang class name to invoice

---
+++ src/Invoicing/Invoice.java
@@ -0,0 +1,25 @@
+package Invoicing;
+
+public class Invoice {
+
+//	public static void main(String[] args) {
+		private String coustomerName;
+		private	Integer phoneNumber;
+		private	Integer invoiceDate;
+		int count;
+		void countItems(int numberOfItemes) {
+//			for(int i=0;i<=numberOfItemes;i++) {
+//				count++;
+//				System.out.println(count);
+//			}
+			System.out.println("Noumber of Items:"+ numberOfItemes);
+		}
+		
+		void totalAmount() {
+			
+		}
+		
+
+	}
+
+
--- src/Invoicing/Items.java
+++ src/Invoicing/Items.java
@@ -1,5 +1,26 @@
 package Invoicing;
 
-public class Items {
+import java.util.HashMap;
 
-}
+public class Items {
+	public static void main(String[] args) {
+		HashMap<String, Float> price=new HashMap<>();
+		price.put("Water package 1.5L",(float) 1 );
+		price.put("Bread",(float) 0.200 );
+		price.put("juce", (float) 0.500);
+		price.put("Detol",(float) 2);
+		
+		System.out.println("Each Itemes with price in OR:"+price);
+		
+//		void unitPrice() {
+//			
+//		}
+		
+		
+		
+		}
+	
+	}
+
+
+
